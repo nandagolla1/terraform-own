cef322d101d601087548f470384aeb012e1a8bed
nanda <nandagolla074@gmail.com>
2025-06-27 09:49:05 +0530

    terraform new

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "helloworld" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow TLS inbound traffic and all outbound traffic"
