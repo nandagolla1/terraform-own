78e0312d676c3b7a9a5b0adcbd931eb32c3053ca
nanda <nandagolla074@gmail.com>
2025-06-27 13:02:09 +0530

    terraform new

---
+++ loops/variables.tf
@@ -0,0 +1,51 @@
+variable "ami_id" {
+  type        = string
+  description = "join devops ami id RHEL9"
+  default     = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "aws_instance_tags" {
+  type = map(string)
+  default = {
+    "Name" = "Roboshop"
+  }
+}
+
+variable "sg_tags" {
+  type = map(string)
+  default = {
+    Name    = "allow_all"
+    Purpose = "security to the instance"
+  }
+}
+
+variable "sg_description" {
+  type    = string
+  default = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+variable "from_port" {
+  default = 0
+}
+
+variable "to_port" {
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "environment" {
+
+}
+
+variable "instances" {
+  type    = list(string)
+  default = ["mongodb", "redis", "mysql", "rabbitmq"]
+}
