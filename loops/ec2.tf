31e48c724d16c5f9e66b6cb3ab128751ab2d9ff9
nanda <nandagolla074@gmail.com>
2025-06-27 13:14:39 +0530

    terraform new

--- loops/ec2.tf
+++ loops/ec2.tf
@@ -1,14 +1,27 @@
-resource "aws_instance" "helloworld" {
-  count                  = 4
-  ami                    = var.ami_id
-  instance_type          = var.environment == "dev" ? "t3.micro" : "t3.small"
+# resource "aws_instance" "helloworld" {
+#   count = 4
+#   ami           = var.ami_id
+#   instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
+#   vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+
+#   tags = {
+#     Name = "Roboshop${count.index}"
+#   }
+# }
+
+resource "aws_instance" "roboshop" {
+  count = length(var.instances)
+  ami           = var.ami_id
+  instance_type = var.instance_type != "mysql" ? "t3.micro" : "t3.small"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
 
   tags = {
-    Name = "Roboshop${count.index}"
-  }
+    Name = var.instances[count.index]
+    }
 }
+
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
