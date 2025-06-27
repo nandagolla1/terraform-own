96a4581c7f9037b7d3e20b7d353e83bf81555fe2
nanda <nandagolla074@gmail.com>
2025-06-27 23:05:27 +0530

    terraform new

---
+++ state/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+    backend "s3" {
+    bucket = "nanda-remote-state"
+    key    = "remote-state"
+    region = "us-east-1"
+    #dynamodb_table = "nanda-remote-state"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
